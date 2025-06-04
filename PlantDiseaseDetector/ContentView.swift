741643199da7f7047eef118d61f08758075d0449
Lariach <chairaloktovian@gmail.com>
2025-06-04 16:12:23 +0700

    Initial Commit

---
+++ PlantDiseaseDetector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlantDiseaseDetector
+//
+//  Created by Chairal Octavyanz on 04/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlantDiseaseDetector/PlantDiseaseDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantDiseaseDetectorApp.swift
+//  PlantDiseaseDetector
+//
+//  Created by Chairal Octavyanz on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PlantDiseaseDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
