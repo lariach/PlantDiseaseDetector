ea89a849d64138eae90588d1648714541b62f928
ricotandrio <ricotandrio@gmail.com>
2025-06-18 13:27:40 +0700

    fix: shadow at disease card

--- PlantDiseaseDetector/Views/Home/PlantDiagnosisCardView.swift
+++ PlantDiseaseDetector/Views/Home/PlantDiagnosisCardView.swift
@@ -11,11 +11,21 @@ struct PlantDiagnosisCardView: View {
     let plantDiagnosis: PlantDiagnosis
     
     let onDelete: () -> Void
+
+    @State private var onDeleteAlertPresented: Bool = false
+    
+    func openDeleteAlert() {
+        onDeleteAlertPresented = true
+    }
+    
+    func closeDeleteAlert() {
+        onDeleteAlertPresented = false
+    }
     
     var body: some View {
         NavigationLink(destination: PlantDiagnosisView(plantDiagnosis: plantDiagnosis)) {
             
-            HStack{
+            HStack {
                 
                 Image(uiImage: plantDiagnosis.getImage())
                     .resizable()
@@ -49,7 +59,7 @@ struct PlantDiagnosisCardView: View {
                     Spacer()
                     
                     Button {
-                        onDelete()
+                        openDeleteAlert()
                     } label: {
                         HStack {
                             Spacer()
@@ -71,7 +81,31 @@ struct PlantDiagnosisCardView: View {
             .frame(maxWidth: 353, maxHeight: 140)
             .background(Color.white)
             .cornerRadius(20)
+            .padding(.vertical, 5)
+            .padding(.horizontal, 5)
             .shadow(color: .black.opacity(0.5), radius: 4, x: 0, y: 4)
+            .alert("Delete Diagnosis", isPresented: $onDeleteAlertPresented) {
+                Button("Cancel", role: .cancel) {
+                    closeDeleteAlert()
+                }
+                Button("Delete", role: .destructive) {
+                    onDelete()
+                }
+            } message: {
+                Text("Are you sure you want to delete this diagnosis?")
+            }
         }
     }
+}
+
+#Preview {
+    PlantDiagnosisCardView(
+        plantDiagnosis: PlantDiagnosis(
+            disease: .rust,
+            probability: 0.95,
+            diseases: [],
+            photo: UIImage(systemName: "photo") ?? UIImage(),
+        ),
+        onDelete: {}
+    )
 }
